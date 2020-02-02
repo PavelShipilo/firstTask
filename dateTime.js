9d1d8e1c2a3a6ba735ed19eb4fb458d21d0e536f
Pavel <shipilopavel92@gmail.com>
2020-02-03 00:09:43 +0300

    1-3 task from datetime lesson

---
+++ dateTime.js
@@ -0,0 +1,57 @@
+/*
+Задание 1
+Создайте объект Date для даты: 20 февраля 2012 года, 3 часа 12 минут. Временная зона – местная.
+Для вывода используйте alert.
+*/
+//Решение
+let customDate = new Date(2012, 1, 20, 3, 12);
+alert(customDate);
+
+/*Задание 2
+Напишите функцию getWeekDay(date), показывающую день недели в коротком формате: «ПН», «ВТ», «СР», «ЧТ», «ПТ», «СБ», «ВС».
+Например:
+let date = new Date(2012, 0, 3);  // 3 января 2012 года
+alert( getWeekDay(date) );        // нужно вывести "ВТ"
+*/
+//Решение
+function getWeekDay(date) {
+    let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+
+    return days[date.getDay()];
+    }
+let date = new Date(2012, 0, 3);
+
+alert( getWeekDay(date) );
+
+/*Задание 2
+В Европейских странах неделя начинается с понедельника (день номер 1), затем идёт вторник (номер 2) и так до воскресенья (номер 7).
+Напишите функцию getLocalDay(date), которая возвращает «европейский» день недели для даты date.
+let date = new Date(2012, 0, 3);  // 3 января 2012 года
+alert( getLocalDay(date) );       // вторник, нужно показать 2
+*/
+// Решение
+ function getLocalDay(date) {
+let day = date.getDay();
+    if (day == 0) {
+      day = 7;
+    }
+    return day;
+}
+/*
+Задание 3
+Создайте функцию getDateAgo(date, days), возвращающую число, которое было days дней назад от даты date.
+К примеру, если сегодня двадцатое число, то getDateAgo(new Date(), 1) вернёт девятнадцатое и getDateAgo(new Date(), 2) – восемнадцатое.
+Функция должна надёжно работать при значении days=365 и больших значениях:
+let date = new Date(2015, 0, 2);
+alert( getDateAgo(date, 1) ); // 1, (1 Jan 2015)
+alert( getDateAgo(date, 2) ); // 31, (31 Dec 2014)
+alert( getDateAgo(date, 365) ); // 2, (2 Jan 2014)
+P.S. Функция не должна изменять переданный ей объект date.
+*/
+//Решение
+function getDateAgo(date, days) {
+  let dateCopy = new Date(date);
+
+  dateCopy.setDate(date.getDate() - days);
+  return dateCopy.getDate();
+}
