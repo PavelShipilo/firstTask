fd2a35eb3929f49900797a615c17e7d68846ceb8
Pavel <shipilopavel92@gmail.com>
2020-02-03 21:38:53 +0300

    finished tasks dateTime

--- dateTime.js
+++ dateTime.js
@@ -55,3 +55,91 @@ function getDateAgo(date, days) {
   dateCopy.setDate(date.getDate() - days);
   return dateCopy.getDate();
 }
+
+/*
+Задание 4
+Напишите функцию getLastDayOfMonth(year, month), возвращающую последнее число месяца. Иногда это 30, 31 или даже февральские 28/29.
+Параметры:
+year – год из четырёх цифр, например, 2012.
+month – месяц от 0 до 11.
+К примеру, getLastDayOfMonth(2012, 1) = 29 (високосный год, февраль).
+*/
+//Решение
+function getLastDayOfMonth(year, month) {
+  let date = new Date(year, month + 1, 0);
+  return date.getDate();
+}
+
+/*
+Задание 5
+Напишите функцию getSecondsToday(), возвращающую количество секунд с начала сегодняшнего дня.
+Например, если сейчас 10:00, и не было перехода на зимнее/летнее время, то:
+getSecondsToday() == 36000 // (3600 * 10)
+Функция должна работать в любой день, т.е. в ней не должно быть конкретного значения сегодняшней даты.
+*/
+//Решение
+function getSecondsToday() {
+  let d = new Date();
+  return d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
+}
+
+/*
+Задание 6
+Создайте функцию getSecondsToTomorrow(), возвращающую количество секунд до завтрашней даты.
+Например, если сейчас 23:00, то:
+getSecondsToTomorrow() == 3600
+P.S. Функция должна работать в любой день, т.е. в ней не должно быть конкретного значения сегодняшней даты.
+*/
+//Решение
+function getSecondsTomorrow() {
+  let d = new Date();
+  let secondsInDay = 86400;
+  let  secondsToday = d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
+  return secondsInDay - secondsToday;
+}
+/*
+Задание 7
+Напишите функцию formatDate(date), форматирующую date по следующему принципу:
+Если спустя date прошло менее 1 секунды, вывести "прямо сейчас".
+В противном случае, если с date прошло меньше 1 минуты, вывести "n сек. назад".
+В противном случае, если меньше часа, вывести "m мин. назад".
+В противном случае, полная дата в формате "DD.MM.YY HH:mm". А именно: "день.месяц.год часы:минуты", всё в виде двух цифр, т.е. 31.12.16 10:00.
+*/
+//Решение
+function formatDate(date){
+  let ms = new Date() - date;
+
+  if (ms < 1000){
+    return 'прямо сейчас';
+  }
+
+  let sec = Math.floor(ms / 1000);
+
+  if (sec < 60) {
+    return sec+ ' сек. назад';
+  }
+
+  let min = Math.floor(ms / 60000);
+
+  if (min < 60) {
+    return min + ' мин. назад'
+  }
+    let d = date;
+    d = [
+      '0' + d.getDate(),
+      '0' + (d.getMonth() + 1),
+      '' + d.getFullYear(),
+      '0' + d.getHours(),
+      '0' + d.getMinutes()
+    ].map(component => component.slice(-2));
+
+    return d.slice(0, 3).join('.') + ' ' + d.slice(3).join(':');
+  }
+
+  alert( formatDate(new Date(new Date - 1)) );
+
+  alert( formatDate(new Date(new Date - 30 * 1000)) );
+
+  alert( formatDate(new Date(new Date - 5 * 60 * 1000)) );
+
+  alert( formatDate(new Date(new Date - 86400 * 1000)) );
