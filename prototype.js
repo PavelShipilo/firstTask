ca50ef37b8483cb53acebc65013c66889d71eed4
Pavel <shipilopavel92@gmail.com>
2020-02-06 22:55:08 +0300

    added some style changes

--- prototype.js
+++ prototype.js
@@ -1,9 +1,9 @@
 //1. Есть объект a с набором свойств, объект b должен унаследовать свойства a
 let animal = {
-  eats: true
+	eats: true
 };
 let rabbit = {
-  jumps: true
+	jumps: true
 };
 
 rabbit.__proto__ = animal;
