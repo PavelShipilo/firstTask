1401bc7860a900a92ecbaaf0630961a738daeaa6
Pavel <shipilopavel92@gmail.com>
2020-03-02 00:10:28 +0300

    made some eddits

--- sileniumTasks/index.js
+++ sileniumTasks/index.js
@@ -1,34 +1,55 @@
-const {Builder, By, Key, util} = require("selenium-webdriver");
+const { Builder, By, Key, util } = require("selenium-webdriver");
 
-const singleton = (function() {
-   
-    let instance;
-    
-     function exampleChrome() {
-        let driver =  new Builder().forBrowser("chrome").build();
-        /*driver.get("https://www.bbc.com/");
-        /*await driver.findElement(By.id("orb-search-q")).sendKeys("selenium");
-        console.log(await driver.findElement(By.id("orb-search-q")).isDisplayed());
-        console.log(await driver.findElement(By.id("orb-search-q")).isEnabled());
-        await driver.findElement(By.xpath('//div[@id="orb-nav-links"]/ul/li[@class="orb-nav-newsdotcom"]/a')).click();*/  
-     }
-    
+const singleton = (function () {
 
-    function createInstance() {
-        return {
-            exampleChrome: exampleChrome,
-         }
-    }
+  let instance;
 
+  async function exampleChrome() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://www.bbc.com/");
+    await driver.findElement(By.id("orb-search-q")).sendKeys("selenium");
+    console.log(await driver.findElement(By.id("orb-search-q")).isDisplayed());
+    console.log(await driver.findElement(By.id("orb-search-q")).isEnabled());
+    await driver.findElement(By.xpath('//div[@id="orb-nav-links"]/ul/li[@class="orb-nav-newsdotcom"]/a')).click();
+  }
+
+  async function exampleFireFox() {
+    let driver = await new Builder().forBrowser("firefox").build();
+    await driver.get("https://www.bbc.com/");
+    await driver.findElement(By.id("orb-search-q")).sendKeys("selenium");
+    console.log(await driver.findElement(By.id("orb-search-q")).isDisplayed());
+    console.log(await driver.findElement(By.id("orb-search-q")).isEnabled());
+    await driver.findElement(By.xpath('//div[@id="orb-nav-links"]/ul/li[@class="orb-nav-newsdotcom"]/a')).click();
+  }
+
+  async function exampleIE() {
+    let driver = await new Builder().forBrowser("internet explorer").build();
+    await driver.get("https://www.bbc.com/");
+    await driver.findElement(By.css('input#orb-search-q')).sendKeys("selenium");
+    console.log(await driver.findElement(By.id("orb-search-q")).isDisplayed());
+    console.log(await driver.findElement(By.id("orb-search-q")).isEnabled());
+    await driver.findElement(By.xpath('//div[@id="orb-nav-links"]/ul/li[@class="orb-nav-newsdotcom"]/a')).click();
+  }
+
+  function createInstance() {
     return {
-      getInstance: function() {
-        
-        return instance || (instance = createInstance());
-
-      }
+      exampleChrome: exampleChrome,
+      exampleFireFox: exampleFireFox,
+      exampleIE: exampleIE
     }
-  })();
+  }
+
+  return {
+    getInstance: function () {
+
+      return instance || (instance = createInstance());
+
+    }
+  }
+}());
 
 
 singleton.getInstance().exampleChrome();
+singleton.getInstance().exampleFireFox();
+singleton.getInstance().exampleIE();
 
